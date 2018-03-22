0ceaf512e1987d9c961d692074d8dbe9cc6229b8
Matthijs Kralt <matthijs29@live.com>
2018-03-22 11:25:58 +0100

    feat: Add all interesting places, finish static design

--- src/Utilities/places.js
+++ src/Utilities/places.js
@@ -1,9 +1,65 @@
 const places = [
   {
-    name: 'Rijnsburg',
+    name: 'The Vliet in Rijnsburg',
     position: {
-      lat: 52.188883,
-      lng: 4.438469
+      lat: 52.191069,
+      lng: 4.445884
+    }
+  },
+  {
+    name: 'Tulip Fields near Voorhout',
+    position: {
+      lat: 52.210548,
+      lng: 4.466760
+    }
+  },
+  {
+    name: 'The Dunes of Katwijk',
+    position: {
+      lat: 52.185892,
+      lng: 4.388444
+    }
+  },
+  {
+    name: 'The Beach of Katwijk',
+    position: {
+      lat: 52.204917,
+      lng: 4.392255
+    }
+  },
+  {
+    name: 'Burcht of Leiden',
+    position: {
+      lat: 52.158915,
+      lng: 4.492542
+    }
+  },
+  {
+    name: 'Pieterskerk in Leiden',
+    position: {
+      lat: 52.157653,
+      lng: 4.487879
+    }
+  },
+  {
+    name: 'Morspoort in Leiden',
+    position: {
+      lat: 52.162021,
+      lng: 4.482111
+    }
+  },
+  {
+    name: 'The House of Spinoza',
+    position: {
+      lat: 52.191470,
+      lng: 4.437146
+    }
+  },
+  {
+    name: 'Castle Endegeest',
+    position: {
+      lat: 52.172722,
+      lng: 4.463108
     }
   }
 ]
